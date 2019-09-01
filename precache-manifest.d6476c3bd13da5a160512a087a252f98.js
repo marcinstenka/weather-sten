018596a1cc1d449b478fde986b34d42ab82fb339
Marcin Stenka <mastenka@wp.pl>
2019-09-01 17:12:51 +0200

    Updates

---
+++ precache-manifest.d6476c3bd13da5a160512a087a252f98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5bbbbdb55932906ea832242cb081c4",
+    "url": "/weather-sten/index.html"
+  },
+  {
+    "revision": "67b652b7f5fdc7481ef4",
+    "url": "/weather-sten/static/css/main.d0a82d85.chunk.css"
+  },
+  {
+    "revision": "15216a4871b859c56051",
+    "url": "/weather-sten/static/js/2.7d68266f.chunk.js"
+  },
+  {
+    "revision": "67b652b7f5fdc7481ef4",
+    "url": "/weather-sten/static/js/main.45d4786b.chunk.js"
+  },
+  {
+    "revision": "d5fab68b4730719dc38f",
+    "url": "/weather-sten/static/js/runtime~main.c802e76f.js"
+  }
+]);
